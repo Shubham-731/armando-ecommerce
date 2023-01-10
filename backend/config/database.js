53a5bf12168a0c23265f0b58530f0403c90de040
Suraj Pandey <shpcore103@gmail.com>
2023-01-10 06:32:22 +0530

    db password

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "ecommerce"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "shubbhu101"),
+      password: env("DATABASE_PASSWORD", "shubbhu101"), // Replace shubbhu101 by <your-db-pasword>
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
